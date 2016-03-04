2a9ff8bdcfde574b0ef0215d34eabeace6d7f026
Dmitry Satrostin <incrop@gmail.com>
2016-03-04 03:20:35 +0300

    server setup

--- make.js
+++ make.js
@@ -6,6 +6,10 @@ var buildDir = 'build';
 var fs = require('fs');
 var bh = new (require('bh').BH);
 
+bh.setOptions({
+    delimMod: '--'
+});
+
 make.build().then(function() {
     eval(fs.readFileSync('./' + bmConf.outdir + '/' + bmConf.outname + '.bh.js', 'utf-8'));
     fs.readdirSync(pagesDir).forEach(function(name) {
